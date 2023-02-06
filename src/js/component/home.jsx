e3cac7e245bc32426842631df97ed0b73f8c1f4b
Daianamelo <melocecilia.daiana@gmail.com>
2023-02-06 03:53:01 +0000

    My first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,94 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+
+	const[input,setInput]=useState("")
+
+	const [todos, setTodos] = useState([])
+
+		function removerItem(index) {
+			const newLista = [...todos];
+			newLista.splice(index, 1);
+			setTodos(newLista);
+			actualizarTodos()
+		}
+		// function removerItem(event) {
+			
+		// 	let borranding = event.target.id
+		// 	console.log(todos)
+		// 	console.log(borranding)
+		// 	setTodos (todos.filter(todos => todos != borranding))
+		// 	}
+
+		function crearUsuario(){
+			fetch(`https://assets.breatheco.de/apis/fake/todos/user/daimelos`,
+			{method: 'POST', 
+			headers: {
+				'Content-Type': 'application/json'},
+			body: JSON.stringify([])
+		})
+		}
+
+		function cargarInput(e) {
+			e.preventDefault()// detenemos el comportamiento predeterminado para procesar nuestro codigo
+			let unToDo = {}
+			unToDo.label = input
+			unToDo.done = false
+			setTodos([...todos, unToDo]);
+			setInput("");
+			actualizarTodos()
+		}
+		function cargarTodos(){
+			fetch(`https://assets.breatheco.de/apis/fake/todos/user/daimelos`,
+			{method: 'GET',})
+			.then((response)=>response.json())
+			.then((data)=>setTodos(data))
+			.then((data)=>console.log(data))
+		}
+	
+	
+		function actualizarTodos(){
+			fetch(`https://assets.breatheco.de/apis/fake/todos/user/daimelos`,
+			{method: 'PUT', 
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(todos)})
+			.then((response)=>response.json())
+		}
+
+		function hechoNoHecho(index) {
+			const newTodos = [...todos];
+			newTodos[index].done = newTodos[index].done === false ? true : false;
+			setTodos(newTodos);
+			actualizarTodos()
+			console.log(todos)
+		  }
+
+		useEffect(() => {
+			crearUsuario()    
+		}, [])
+
+		useEffect(() => {
+			cargarTodos()    
+		}, [])
+
+		return (
+			<>
+			<form className="container" onSubmit={cargarInput}>
+	  <div className="mb-3">
+		<img src="https://desktime.com/blog/wp-content/uploads/2021/03/feature.png"></ img>
+		{/*2. definimos el evento ochange en el input */}
+		<input type="text" className="form-control" id="elInput" aria-describedby="emailHelp" onChange={(e)=>{setInput(e.target.value)}} value={input}></ input>
+		<div id="inputList" className="form-text">
+			<ul className="list-group">
+				{todos.map((element, index) => <div className="row" key={index}><li className="list-group-item col-10">{element.label}</li><button type="button" className={`btn btn-outline-${todos[index].done === true ? 'success' : 'danger'} col-1`} onClick={() => hechoNoHecho(index)}> {todos[index].done === true ? <i className="fa fa-check" /> : <i className="fa fa-times" />}</button><button type="button" className="btn btn-outline-danger col-1" onClick={() => removerItem(index)}><i className="fas fa-trash-alt"></i></button></div>)}
+			</ul>
+		</div>
+	  </div>
+	</form>
+		
+	</>
+		);
+	};
+	
+	export default Home;
